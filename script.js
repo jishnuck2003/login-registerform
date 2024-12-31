86d00f148bcd56577bf0c943b2be64ecfedfcd93
jishnu <jishnu3002@gmail.com>
2024-12-31 14:26:13 +0530

    second commit

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 document.getElementById("loginBtn").addEventListener("click", function() {
     document.querySelector(".popup").style.display = "flex";
+    document.querySelector(".popup").style.transition = "opacity 3s";
+    document.querySelector(".popup").style.opacity = "1";
 });
+
 
 document.querySelector(".close-btn").addEventListener("click", function() {
     document.querySelector(".popup").style.display = "none";
 });
 
-document.querySelector("registerBtn").addEventListener("click", function() {
-    document.querySelector("#loginpopup").style.display = "none";
-    document.querySelector("#registerpopup").style.display = "flex";
+document.getElementById('registerBtn').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('loginpopup').style.display = 'none';
+    document.getElementById('registerpopup').style.display = 'block';
 });
 
-document.querySelector(".close-btn").addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "none";
+document.querySelectorAll('.close-btn').forEach(btn => {
+    btn.addEventListener('click', function() {
+        this.parentElement.parentElement.style.display = 'none';
+    });
 });
